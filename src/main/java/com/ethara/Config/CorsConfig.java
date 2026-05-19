6c175b7d0b76f766a803232b8ef0d4e4eb01e0b4
rakesh-ethara <rakesh.kumar143@ethara.ai>
2026-05-19 15:20:23 +0530

    Fixed CORS configuration

--- src/main/java/com/ethara/Config/CorsConfig.java
+++ src/main/java/com/ethara/Config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
